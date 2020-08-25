f3115d9cee78e1fac53a2a88d9f5884b0db17f79
Zac <zd.holmes@live.com>
2020-08-25 15:00:42 +1200

    testing

--- SoftwareProcess/Startup.cs
+++ SoftwareProcess/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SoftwareProcess.Data;
-
+// Hi 
 namespace SoftwareProcess
 {
     public class Startup
