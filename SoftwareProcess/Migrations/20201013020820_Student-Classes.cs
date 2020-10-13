9694be6c2ae4ec4c3aebd7b9696b25422a70fbd2
Zac <zd.holmes@live.com>
2020-10-13 15:48:42 +1300

    StudentClass added, added to admin interface

---
+++ SoftwareProcess/Migrations/20201013020820_Student-Classes.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SoftwareProcess.Migrations
+{
+    public partial class StudentClasses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.CreateTable(
+                name: "StudentClasses",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClassID = table.Column<int>(nullable: false),
+                    StudentID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentClasses", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentClasses");
+
+           
+        }
+    }
+}
---
+++ SoftwareProcess/Models/StudentClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftwareProcess.Models
+{
+    public class StudentClass
+    {
+        public int ID { get; set; }
+
+        public int ClassID { get; set; }
+
+        public int StudentID { get; set; }
+    }
+}
